c8a7e44492edd11246af80b2daef7124996e09a8
n.gurbanov <nariman.qurbanov@idda.az>
2024-04-05 14:11:47 +0400

    JDBC

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        TeacherService teacherService = new TeacherService();
+
+        teacherService.showMenu();
+
+    }
+}
